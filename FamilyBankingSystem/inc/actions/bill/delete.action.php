a5288550af81e016e01f7f0d63eac347ff17243b
= <=>
2023-03-16 02:05:33 +0500

    delete

--- FamilyBankingSystem/inc/actions/bill/delete.action.php
+++ FamilyBankingSystem/inc/actions/bill/delete.action.php
@@ -1,3 +1,9 @@
 <?php 
-include_once "inc/session/sessioncheck.php";
-include_once "inc/db/connection.php";
+include_once "../../session/sessioncheck.php";
+include_once "../../db/connection.php";
+
+$id = $db->__escape($_GET["id"]);
+
+$sql = "DELETE FROM bill where id = $id";
+$db->__execute($sql);
+header("Location: ../../../bills.php");
--- FamilyBankingSystem/inc/actions/members/delete.action.php
+++ FamilyBankingSystem/inc/actions/members/delete.action.php
@@ -1,3 +1,9 @@
 <?php 
 include_once "../../session/sessioncheck.php";
 include_once "../../db/connection.php";
+
+$id = $db->__escape($_GET["id"]);
+
+$sql = "DELETE FROM users where id = $id";
+$db->__execute($sql);
+header("Location: ../../../members.php");
