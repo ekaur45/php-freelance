03a065f2a00ad4e72c01c4a3b9c697c9cabf632b
waqas.ahmad <waqas.ahmad@wisemani.com>
2023-03-15 19:28:38 +0500

    db

--- FamilyBankingSystem/database.sql
+++ FamilyBankingSystem/database.sql
@@ -1,18 +1,158 @@
--- Active: 1677665319572@@127.0.0.1@3306@test
-CREATE DATABASE `familybanking`;
-
+CREATE DATABASE  IF NOT EXISTS `familybanking` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `familybanking`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: familybanking
+-- ------------------------------------------------------
+-- Server version	8.0.32
 
-CREATE Table `users`
-(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    firstName VARCHAR(500),
-    lastName VARCHAR(500),
-    username VARCHAR(500),
-    dataOfBirth DATETIME,
-    mobileNumber VARCHAR(50),
-    `password` VARCHAR(500),
-    userType ENUM('Main user','Basic user'),
-    createAt DATETIME DEFAULT now(),
-    isDeleted BIT(1)
-)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bill`
+--
+
+DROP TABLE IF EXISTS `bill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bill` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `billType` varchar(255) DEFAULT NULL,
+  `month` varchar(255) DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `status` enum('Not available','Pending','Partial paid','Paid') DEFAULT NULL,
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `isDeleted` bit(1) DEFAULT NULL,
+  `amountPaid` decimal(10,2) DEFAULT NULL,
+  `Year` varchar(45) DEFAULT NULL,
+  `userId` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bill`
+--
+
+LOCK TABLES `bill` WRITE;
+/*!40000 ALTER TABLE `bill` DISABLE KEYS */;
+INSERT INTO `bill` VALUES (1,'Water','March',100.00,'Pending','2023-03-15 18:04:44',NULL,0.00,'2023',NULL),(2,'Water','March',100.00,'Pending','2023-03-15 18:06:14',NULL,0.00,'2023',1);
+/*!40000 ALTER TABLE `bill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `transactionType` varchar(255) DEFAULT NULL,
+  `userId` int DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `transactionDetail` varchar(500) DEFAULT NULL,
+  `isDebit` bit(1) DEFAULT NULL,
+  `cretedAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `isDeleted` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(500) DEFAULT NULL,
+  `lastName` varchar(500) DEFAULT NULL,
+  `username` varchar(500) DEFAULT NULL,
+  `dataOfBirth` datetime DEFAULT NULL,
+  `mobileNumber` varchar(50) DEFAULT NULL,
+  `password` varchar(500) DEFAULT NULL,
+  `userType` enum('Main user','Basic user') DEFAULT NULL,
+  `createAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `isDeleted` bit(1) DEFAULT NULL,
+  `belongsTo` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'sadfasdf','sdafsda','360poc','2023-03-15 00:00:00','asdfsda','ae176a462de733eef53d680fbbda7f73','Main user','2023-03-15 15:04:00',NULL,NULL),(2,'Abdul','Basit','basit','2023-03-15 00:00:00','111111111','25f9e794323b453885f5181f1b624d0b','Basic user','2023-03-15 16:13:13',NULL,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `voucher`
+--
+
+DROP TABLE IF EXISTS `voucher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `voucher` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `voucherName` varchar(255) DEFAULT NULL,
+  `applicableAmount` decimal(10,2) DEFAULT NULL,
+  `assignedTo` int DEFAULT NULL,
+  `applicableDate` datetime DEFAULT NULL,
+  `isConsumed` bit(1) DEFAULT NULL,
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `isDeleted` bit(1) DEFAULT NULL,
+  `userId` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `voucher`
+--
+
+LOCK TABLES `voucher` WRITE;
+/*!40000 ALTER TABLE `voucher` DISABLE KEYS */;
+INSERT INTO `voucher` VALUES (1,'Birthday',20.00,2,'2023-03-15 00:00:00',NULL,'2023-03-15 19:01:57',NULL,1);
+/*!40000 ALTER TABLE `voucher` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'familybanking'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-15 19:27:41
